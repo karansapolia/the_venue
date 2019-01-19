1a17ceba446be32f415f9ce600965399dffd56a1
Karan Sapolia <sapoliakaran@gmail.com>
2019-01-20 05:11:04 +0530

    feat: building the application

---
+++ src/components/header_footer/SideDrawer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+const SideDrawer = (props) => {
+    return (
+        <Drawer
+            anchor='right'
+            open={props.open}
+            onClose={() => props.onClose(false)}
+        >
+            <List component='nav'>
+                <ListItem button onClick={() => console.log('clicked')}>
+                    Event Starts in:
+                </ListItem>
+                <ListItem button onClick={() => console.log('clicked')}>
+                    Venue Info
+                </ListItem>
+                <ListItem button onClick={() => console.log('clicked')}>
+                    Highlights
+                </ListItem>
+                <ListItem button onClick={() => console.log('clicked')}>
+                    Pricing
+                </ListItem>
+                <ListItem button onClick={() => console.log('clicked')}>
+                    Location
+                </ListItem>
+            </List>
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
